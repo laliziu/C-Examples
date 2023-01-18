c8e24e27d363bffc8228011a44428db9dd8b6fa7
Lali <lalizyulkorn@gmail.com>
2023-01-19 01:07:41 +0400

    add example005 if or esle

---
+++ Examples/Example005_iforelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+ {
+    Console.WriteLine("Yes, it is MASHA");
+ }
+
+else
+{
+    Console.WriteLine("Hello, "); 
+    Console.WriteLine(username);
+}
